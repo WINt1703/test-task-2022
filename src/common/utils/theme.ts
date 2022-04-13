9e40c3cebe872e0aad8f3778fbae4807d2ccbd17
wint1703 <isuszanty@gmail.com>
2022-04-13 12:40:32 +0300

    added theme

---
+++ src/common/utils/theme.ts
@@ -0,0 +1,49 @@
+import {createTheme} from "@mui/material";
+
+export default createTheme({
+    palette: {
+        background: {
+            default: "#F8F8F8",
+        },
+        primary: {
+            main: "#F4E041",
+        },
+        secondary: {
+            main: "#00BDD3",
+        },
+    },
+    typography: {
+        fontFamily: "Nunito",
+        h1: {
+            fontSize: "40px",
+            lineHeight: "40px",
+            fontWeight: "400"
+        },
+        allVariants: {
+            fontSize: "16px",
+            lineHeight: "26px",
+            fontWeight: "400"
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: "none",
+                    width: 100,
+                    height: 34,
+                    background: "#F4E041",
+                    color: "rgba(0, 0, 0, 0.87)",
+                    borderRadius: 80,
+                    "&:hover": {
+                        background: "#FFE302",
+                    },
+                    "&:disabled": {
+                        background: "#B4B4B4",
+                        color: "rgba(255, 255, 255, 0.87)",
+                    },
+                }
+            }
+        }
+    }
+});
