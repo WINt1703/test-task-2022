5d755481e8606bdc63d11ab2fbd8ff34a60c3429
wint1703 <isuszanty@gmail.com>
2022-04-13 12:34:20 +0300

    init project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['frontend-test-assignment-api.abz.agency'],
+  },
 }
 
 module.exports = nextConfig
